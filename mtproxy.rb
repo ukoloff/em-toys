2e8bae41e2b38b09a61bd601d87741fb08a46b50
Stas Ukolov <ukoloff@gmail.com>
2015-03-15 21:01:51 +0500

    Redo multithreaded proxy

--- mtproxy.rb
+++ mtproxy.rb
@@ -1,22 +1,24 @@
 require 'Socket'
 
+Thread.abort_on_exception=true
+
 puts "Proxy is listening..."
 
 Socket.tcp_server_loop 'localhost', 8082 do |client, addr|
   puts "Connected from #{addr.ip_address}:#{addr.ip_port}"
-  Thread.new client do |client|
+  t1=Thread.new client do |client|
     srv=nil
-    Thread.new do |t|
-      puts "Connecting to server..."
-      srv=Socket.tcp 'localhost', 8081
-      puts "Connected to server"
+    puts "Connecting to server..."
+    srv=Socket.tcp 'localhost', 8081
+    puts "Connected to server"
+    t2=Thread.new do
       until srv.eof do
         s=srv.readpartial 4096
         puts "< #{s.length}"
         client.write s
       end
       puts "Server closed"
-      client.close
+      t1.exit
     end
     until client.eof
       s=client.readpartial 4096
@@ -24,7 +26,6 @@ Socket.tcp_server_loop 'localhost', 8082 do |client, addr|
       srv.write s
     end
     puts "Client closed"
-    client.close
-    srv.close if srv
+    t2.exit
   end
 end
