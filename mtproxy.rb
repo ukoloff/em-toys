2b8128b4e37de747c696ff028a48bef9228b2378
Stas Ukolov <ukoloff@gmail.com>
2015-03-22 20:32:01 +0500

    Modularize Proxy

--- mtproxy.rb
+++ mtproxy.rb
@@ -2,30 +2,47 @@ require 'Socket'
 
 Thread.abort_on_exception=true
 
-puts "Proxy is listening..."
+class Proxy
+  Chunk=0x10000
 
-Socket.tcp_server_loop 'localhost', 8082 do |client, addr|
-  puts "Connected from #{addr.ip_address}:#{addr.ip_port}"
-  t1=Thread.new client do |client|
-    srv=nil
-    puts "Connecting to server..."
-    srv=Socket.tcp 'localhost', 8081
-    puts "Connected to server"
-    t2=Thread.new do
-      until srv.eof do
-        s=srv.readpartial 4096
-        puts "< #{s.length}"
-        client.write s
-      end
-      puts "Server closed"
-      t1.exit
+  def self.run!
+    puts "Proxy is listening..."
+    Socket.tcp_server_loop 'localhost', 8082 do |client, addr|
+      puts "Connected from #{addr.ip_address}:#{addr.ip_port}"
+      self.new client
     end
-    until client.eof
-      s=client.readpartial 4096
+  end
+
+  def initialize client
+    @client=client
+    @t1=Thread.new{cloop}
+  end
+
+  # Client loop
+  def cloop
+    puts "Connecting to server..."
+    @server=Socket.tcp 'localhost', 8081
+    puts "Connected to server"
+    @t2=Thread.new{sloop}
+    until @client.eof
+      s=@client.readpartial Chunk
       puts "> #{s.length}"
-      srv.write s
+      @server.write s
     end
     puts "Client closed"
-    t2.exit
+    @t2.exit
   end
+
+  # Server loop
+  def sloop
+    until @server.eof do
+      s=@server.readpartial Chunk
+      puts "< #{s.length}"
+      @client.write s
+    end
+    puts "Server closed"
+    @t1.exit
+  end
+
+  run!
 end
