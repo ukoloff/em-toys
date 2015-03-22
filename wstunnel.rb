7ec3deb3778fbd08654cba3d7593aa0d6ff228af
Stas Ukolov <ukoloff@gmail.com>
2015-03-22 21:06:05 +0500

    Modularize WSTunnel

--- wstunnel.rb
+++ wstunnel.rb
@@ -1,64 +1,92 @@
 require 'Socket'
-#require 'openssl'
-require 'openssl/win/root'
+require 'openssl'
+require 'openssl/win/root' if Gem.win_platform?
 
-Host='ya.ru'
+class Tunnel
+  Host='ya.ru'
 
-puts "WSTunnel is listening..."
+  Chunk=0x10000
 
-Thread.abort_on_exception=true
-
-def reheaders headers
-  return headers if headers.length<1
-  verb=headers.shift
-  [verb]+
-  %w(Host Origin).map{|h| "#{h}: #{Host}"}+
-  headers.reject{|h| /^(?:host|origin):/i.match h}
-end
-
-def read_headerz stream
-  r=[]
-  until stream.eof
-    s=stream.gets.strip
-    break if 0==s.length
-    r << s
-  end
-  r
-end
-
-def req client
-  headers=reheaders  read_headerz client
-  puts "New headers: ", headers.map{|h| "\t#{h}"}
-
-  puts "Connecting to server..."
-  srv=Socket.tcp Host, 443
-  ctx=OpenSSL::SSL::SSLContext.new
-  ctx.set_params verify_mode: OpenSSL::SSL::VERIFY_PEER
-  srv=OpenSSL::SSL::SSLSocket.new srv, ctx
-  srv.hostname=Host if srv.respond_to? :hostname=
-  srv.connect
-  puts "Connected to server; #{srv.verify_result}"
-  srv.write headers*"\r\n"+"\r\n\r\n"
-  Thread.new do |t|
-    until srv.eof do
-      s=srv.readpartial 4096
-      # puts "< #{s.length}"
-      client.write s
+  def self.run!
+    puts "WSTunnel is listening..."
+    Socket.tcp_server_loop 'localhost', 8082 do |client, addr|
+      puts "Connected from #{addr.ip_address}:#{addr.ip_port}"
+      new client
     end
-    puts "Server closed"
-    client.close
   end
-  until client.eof
-    s=client.readpartial 4096
-    # puts "> #{s.length}"
-    srv.write s
-  end
-  puts "Client closed"
-  client.close
-  srv.close if srv
-end
 
-Socket.tcp_server_loop 'localhost', 8082 do |client, addr|
-  puts "Connected from #{addr.ip_address}:#{addr.ip_port}"
-  Thread.new{req client}
+  def initialize client
+    @client=client
+    @t1=Thread.new{cloop!}
+  end
+
+  def cloop!
+    begin
+      puts "<Client>"
+      cloop
+    rescue=>e
+      puts "Client error: #{e}"
+    ensure
+      puts "</Client>"
+      @client.close
+      @t2.exit if @t2
+    end
+  end
+
+  def headerz
+    r=[]
+    until @client.eof
+      s=@client.gets.strip
+      break if 0==s.length
+      r << s
+    end
+    r
+  end
+
+  def headerz! headers
+    return headers if headers.length<1
+    verb=headers.shift
+    [verb]+
+    %w(Host Origin).map{|h| "#{h}: #{Host}"}+
+    headers.reject{|h| /^(?:host|origin):/i.match h}
+  end
+
+  def connect!
+    srv=Socket.tcp Host, 443
+    ctx=OpenSSL::SSL::SSLContext.new
+    ctx.set_params verify_mode: OpenSSL::SSL::VERIFY_PEER
+    srv=OpenSSL::SSL::SSLSocket.new srv, ctx
+    srv.hostname=Host if srv.respond_to? :hostname=
+    srv.connect
+    puts "Connected to server; #{srv.verify_result}"
+    srv
+  end
+
+  def cloop
+    h=headerz! headerz
+    puts "New headers: ", h.map{|h| "\t#{h}"}
+    @server=connect!
+    @server.write h*"\r\n"+"\r\n"*2
+    @t2=Thread.new{sloop!}
+    @server.write @client.readpartial Chunk until @client.eof
+  end
+
+  def sloop!
+    begin
+      puts "<Server>"
+      sloop
+    rescue=>e
+      puts "Server error: #{e}"
+    ensure
+      puts "</Server>"
+      @server.close
+      @t1.exit
+    end
+  end
+
+  def sloop
+    @client.write @server.readpartial Chunk until @server.eof
+  end
+
+  run!
 end
